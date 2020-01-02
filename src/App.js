fc1dfe1a3e76d4599efc2fd085a95973d74f1936
Marino Ali-Bin <marinoali-bin@macbookpro-mab.local>
2020-01-02 04:31:30 +0100

    Mise a jour du menu pour naviguer grace a reactRouter et withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react"
 import HomePage from "./pages/homepage.component"
+import { Switch, Route } from "react-router-dom"
 
 import "./App.css"
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
+
 function App() {
-  return <HomePage />
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
